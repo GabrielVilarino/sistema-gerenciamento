da41eb614d8ea4a646b8664c1e3f7b6f0570e39c
GabrielVilarino <gabriel_vilarino@hotmail.com>
2025-02-26 21:52:23 -0300

    [FIX] alteração conexao back

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://127.0.0.1:8000/:path*'
+                destination: 'https://sistema-gerenciamento-back.onrender.com/:path*'
             }
         ]
     }
